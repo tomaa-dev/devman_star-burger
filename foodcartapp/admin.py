6552ccb7d5e82facab9ff09ac6dda7c9ba176513
tomaa-dev <lajpanovatoma@gmail.com>
2026-09-17 10:03:42 +0300

    Redirect back to manager UI after order edit

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.shortcuts import redirect
 
 from .models import Product
 from .models import ProductCategory
@@ -122,3 +124,18 @@ class OrderAdmin(admin.ModelAdmin):
     search_fields = ('firstname', 'lastname', 'phonenumber', 'address')
     list_filter = ('address',)
     ordering = ('-id',)
+
+    def response_change(self, request, obj):
+        response = super().response_change(request, obj)
+
+        next_url = request.GET.get('next')
+        if not next_url:
+            return response
+
+        if url_has_allowed_host_and_scheme(
+            url=next_url,
+            allowed_hosts={request.get_host()},
+            require_https=request.is_secure(),
+        ):
+            return redirect(next_url)
+        return response
